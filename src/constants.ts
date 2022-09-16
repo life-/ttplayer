b083efacc54a5bf73127c48a899f8bc4c0f7bf75
elmer <elmer.hit@gmail.com>
2022-09-16 17:50:15 +0800

    remove files greater than 25MiB

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,5 @@
 import dukou from './assets/dukou.flac';
-import guyongzhe from './assets/guyongzhe.flac';
 import zheshijienameduoren from './assets/zheshijienameduoren.flac';
-import newThang from './assets/new-thang.flac';
 import gao from './assets/gao.flac';
 import kong from './assets/kong.flac';
 
@@ -16,16 +14,8 @@ export const defaultPlayList: PlayListItem[] = [
     url: dukou,
   },
   {
-    name: '陈奕讯 - 蛄蛹者',
-    url: guyongzhe,
-  },
-  {
     name: '莫文蔚 - 这世界那么多人',
     url: zheshijienameduoren,
-  },
-  {
-    name: 'Redfoo - New Thang',
-    url: newThang,
   },
   {
     name: 'RADWIMPS - 陽菜と、走る帆高',
